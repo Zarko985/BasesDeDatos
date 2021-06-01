781af5208f39ed7486405127d3322f8e08bb1a35
Jorge Lozano <jorgelozanolopez12@gmail.com>
2021-06-01 12:30:00 +0200

    Retoques Visuales y Mejora de Agile

--- SQLITE/Assets/Script/SaveGameManager.cs
+++ SQLITE/Assets/Script/SaveGameManager.cs
@@ -7,13 +7,18 @@ using UnityEngine;
 
 public class SaveGameManager : MonoBehaviour
 {
+    //Creación del Array
     public Ammo[] municion;
     void Update()
     {
+
+        #region Guardado
+        //Al pulsar "S", se guardan los datos
         if (Input.GetKeyDown(KeyCode.S))
         {
             JObject jSaveGame = new JObject();
-           
+
+           //Lee y guarda la informacion en el Array
             for (int i = 0; i < municion.Length; i++)
             {
                 Ammo addAmmo = municion[i];
@@ -23,38 +28,52 @@ public class SaveGameManager : MonoBehaviour
 
             }
             Debug.Log("Saving file");
+
+            //Direccion de guardado
             string filePath = Application.persistentDataPath + "/saveAmmo.sav";
 
+            //Al guardar la informacion la encripta
             byte[] encryptedMessage = Encrypt(jSaveGame.ToString());
             File.WriteAllBytes(filePath, encryptedMessage);
         }
+        #endregion
 
+        #region Carga
+        //Al pulsar "L", carga la informacion
         if (Input.GetKeyDown(KeyCode.L))
         {
+            //Direccion de donde Carga la informacion
             string filePath = Application.persistentDataPath + "/saveAmmo.sav";
             Debug.Log("Loading from: " + filePath);
 
+            //Desencripta la informacion
             byte[] decryptedMessage = File.ReadAllBytes(filePath);
             string jsonString = Decrypt(decryptedMessage);
 
 
             JObject jSaveGame = JObject.Parse(jsonString);
 
+            //Lee la informacion y la extrae
             for (int i = 0; i < municion.Length; i++)
             {
                 Ammo addAmmo = municion[i];
                 string AmmoJsonString = jSaveGame[addAmmo.name].ToString();
                 addAmmo.Deserialize(AmmoJsonString);
             }
+            //Muestra la informacion guardada
             Debug.Log(jSaveGame);
 
 
         }
+        #endregion
     }
 
+    #region Encriptado
+    //Encriptacion de Seguridad
     byte[] _key = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
     byte[] _inicializationVector = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
 
+    //Encripta la informacion guardada
     byte[] Encrypt(string message)
     {
         AesManaged aes = new AesManaged();
@@ -73,6 +92,7 @@ public class SaveGameManager : MonoBehaviour
         return memoryStream.ToArray();
     }
 
+    //Desencripta la informacion para su extraccion
     string Decrypt(byte[] message)
     {
         AesManaged aes = new AesManaged();
@@ -90,4 +110,5 @@ public class SaveGameManager : MonoBehaviour
 
         return decryptedMessage;
     }
+    #endregion
 }
--- SQLITE/Assets/Script/SimpleDB.cs
+++ SQLITE/Assets/Script/SimpleDB.cs
@@ -9,9 +9,14 @@ using UnityEngine.UI;
 public class SimpleDB : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //Nombre de la base de datos
     private string dbName = "URI=file:Inventory.db";
 
+    //Texto para mostrar los datos guardados
     public Text ammoText;
+
+    //Activacion de tabla y datos
     void Start()
     {
         CreateDB();
@@ -23,12 +28,16 @@ public class SimpleDB : MonoBehaviour
 
     }
 
+    #region Base de datos
+    //Creacion de la base de datos
     public void CreateDB()
     {
+        //Conexion con la base de datos
         using (var connection = new SqliteConnection(dbName))
         {
             connection.Open();
 
+            //Crea una tabla llamada Municion en caso de que no exista
             using(var command = connection.CreateCommand())
             {
                 command.CommandText = "CREATE TABLE IF NOT EXISTS Municion (Nombre VARCHAR(20), Cantidad INT);";
@@ -38,26 +47,36 @@ public class SimpleDB : MonoBehaviour
             connection.Close();
         }
     }
+    #endregion
 
+    #region Añadir municion
+    //Metodo para añadir municion
     public void AddAmmo(string AmmoName, int AmmoAmount)
     {
         using (var connection = new SqliteConnection(dbName))
         {
             connection.Open();
-
+            //Activa un objeto para controlar la Base de datos
             using (var command = connection.CreateCommand())
             {
+                //Comando para insertar los valores en la base de datos
                 command.CommandText = "INSERT INTO Municion (Nombre, Cantidad) VALUES ('" + AmmoName + "', '" + AmmoAmount + "');";
                 command.ExecuteNonQuery();
             }
 
             connection.Close();
         }
+        //Actualiza el inventario
         DisplayAmmo();
     }
+    #endregion
 
+    #region Mostrar Municion
+
+    //Muestra las municiones
     public void DisplayAmmo()
     {
+        //Limpia el texto
         ammoText.text = "";
 
         using (var connection = new SqliteConnection(dbName))
@@ -66,12 +85,16 @@ public class SimpleDB : MonoBehaviour
 
             using (var command = connection.CreateCommand())
             {
+                //Selecciona los parametros que van a mostarse
                 command.CommandText = "SELECT * FROM Municion;";
+
 
                 using(IDataReader reader = command.ExecuteReader())
                 {
                     while(reader.Read())
                     
+                        //Muestra en pantalla los campos nombrados
+                        //Muestra tantos como hay guardados
                         ammoText.text += ("Name: " + reader["Nombre"] + "\t\t"+ "Amount: " + reader["Cantidad"] + "\n");
                     
                     reader.Close();
@@ -83,7 +106,11 @@ public class SimpleDB : MonoBehaviour
             connection.Close();
         }
     }
+    #endregion 
 
+    #region munciones
+    //Metodos para la activacion de los botones 
+    //para guardar los datos en la base de datos
     public void Perfrotante()
     {
         AddAmmo("Perforante", 20);
@@ -98,5 +125,5 @@ public class SimpleDB : MonoBehaviour
     {
         AddAmmo("Incendiaria", 5);
     }
-
+    #endregion
 }
